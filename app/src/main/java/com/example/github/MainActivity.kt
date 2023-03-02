46579c95947786e9f4d1f52eb06501e8c4f1092b
Mohamed Majayda <abo.ashraf169@gmail.com>
2023-03-02 16:00:44 +0200

    SecandCommit

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // When Edit Data
+        val x = 10
+        val y = 20
+        var z = x+y
     }
+
 }
